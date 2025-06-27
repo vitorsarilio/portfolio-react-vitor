5cc7d046afe2ed39369f3f9bafadfca724d5f046
Vitor Sarilio <vitorsarilio@hotmail.com>
2025-06-27 11:02:47 -0300

    SEO e thumbnail criadas

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -12,6 +12,13 @@ export default function PortfolioPage() {
   const { trackEvent } = useOutletContext();
   return (
     <>
+      <title>Vitor Hugo Sarilio | Portfólio de Analista de Dados</title>
+      <meta name="description" content="Analista de dados especialista em Business Intelligence (BI), SQL, Python, Power BI e automação de pipelines com Apache Airflow." />
+      <meta property="og:title" content="Vitor Hugo Sarilio | Portfólio de Analista de Dados" />
+      <meta property="og:description" content="Projetos e experiência em análise e engenharia de dados. Conheça meu trabalho." />
+      <meta property="og:url" content="https://www.vitorsarilio.com/"/>
+      <meta property="og:image" content="https://www.vitorsarilio.com/thumbnail.png" />
+      <meta property="twitter:card" content="summary_large_image" />
       <AboutSection user={user} trackEvent={trackEvent} />
       <ProjectsSection projects={projects} trackEvent={trackEvent} />
       <PersonalProjectsSection trackEvent={trackEvent}/>
